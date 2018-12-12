6a04260829f5ee7e948a7ab6fa65a8f8dca50c03
William <william@fontrip.com>
2018-12-12 18:11:40 +0800

    修改中介層

---
+++ app/Http/Middleware/AddHeaders.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+class AddHeaders
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response
+            ->header('Access-Control-Allow-Origin', env('ACCESS_CONTROL_ALLOW_ORIGIN', '*'))
+            ->header('Access-Control-Allow-Methods',  env('ACCESS_CONTROL_ALLOW_METHODS', '*'))
+            ->header('Access-Control-Allow-Headers', env('ACCESS_CONTROL_ALLOW_HEADERS', 'Content-Type'));
+        return $response;
+    }
+}
