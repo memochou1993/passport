61ed6fb699cf2b39a0905d48792209a7debebd7b
Memo Chou <38420282+memochou1993@users.noreply.github.com>
2019-02-12 14:54:23 +0800

    Update AddHeaders.php

--- app/Http/Middleware/AddHeaders.php
+++ app/Http/Middleware/AddHeaders.php
@@ -8,7 +8,7 @@ class AddHeaders
         $response = $next($request);
         $response
             ->header('Access-Control-Allow-Origin', env('ACCESS_CONTROL_ALLOW_ORIGIN', '*'))
-            ->header('Access-Control-Allow-Methods',  env('ACCESS_CONTROL_ALLOW_METHODS', '*'))
+            ->header('Access-Control-Allow-Methods', env('ACCESS_CONTROL_ALLOW_METHODS', '*'))
             ->header('Access-Control-Allow-Headers', env('ACCESS_CONTROL_ALLOW_HEADERS', 'Content-Type'));
         return $response;
     }
